68963a0a083449ddd97b6cc8e6e47d1c076c0104
Rodrigo Appelt <rappelt@inf.ufsm.br>
2024-02-25 18:43:30 -0300

    ping support

--- src/Server.cs
+++ src/Server.cs
@@ -1,10 +1,16 @@
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
+
+const int PORT = 6379;
+const string PING_RESPONSE = "+PONG\r\n";
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 Console.WriteLine("Logs from your program will appear here!");
 
 // Uncomment this block to pass the first stage
-TcpListener server = new TcpListener(IPAddress.Any, 6379);
+TcpListener server = new TcpListener(IPAddress.Any, PORT);
 server.Start();
-server.AcceptSocket(); // wait for client
+Socket socket = server.AcceptSocket(); // wait for client
+
+socket.Send(Encoding.ASCII.GetBytes(PING_RESPONSE));
